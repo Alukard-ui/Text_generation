dce5ea8d5687f139090d3bcbddb9a49a42022e06
profi <viacheslavovich.nikita@gmail.com>
2021-04-16 12:33:17 +0300

    add generate text

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import zipfile
 # for filename in zfile.namelist():
 #     zfile.extract(filename)
 from pprint import pprint
+from random import randint
 
-file_name = str(input('Your file name'))
+file_name = str(input('Your file name \n'))
 
 stat = {} # create dictionary for statistic
 # example start = {'п':{'р':500,'ё':5,},'р':{'н':250,'й':1000,'о':1,}}
@@ -28,12 +29,32 @@ with open(file_name,'r',encoding='utf-8') as file:
             else:
                 stat[prev_char] = {char:1}
             prev_char = char # in the previous char we enter the one that was in front of it
-pprint(stat)
 
 totals = {} # dictionary to count all char for prev_char
+stat_for_generate = {}
 for prev_char, char_stat in stat.items():
     totals[prev_char] = 0
+    stat_for_generate[prev_char] = []
     for char,count in char_stat.items():
         totals[prev_char] += count
+        stat_for_generate[prev_char].append([count,char])
+    stat_for_generate[prev_char].sort(reverse=True)
 
-pprint(totals)
+
+
+N = 1000 # volume char
+printed = 0
+
+prev_char =' '
+while printed < N:
+    char_stat = stat_for_generate[prev_char]
+    total = totals[prev_char]
+    dice = randint(1,total)
+    pos = 0
+    for count,char in char_stat:
+        pos += count
+        if dice <= pos:
+            break
+    print(char,end='')
+    printed += 1
+    prev_char = char

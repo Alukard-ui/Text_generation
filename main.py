32c2c011669848a041a7a9a81bc1a40fda939c9f
profi <viacheslavovich.nikita@gmail.com>
2021-04-16 13:37:02 +0300

    format text output and analysis sequence

--- main.py
+++ main.py
@@ -14,47 +14,54 @@ from random import randint
 
 file_name = str(input('Your file name \n'))
 
-stat = {} # create dictionary for statistic
+stat = {}  # create dictionary for statistic
 # example start = {'п':{'р':500,'ё':5,},'р':{'н':250,'й':1000,'о':1,}}
 
-prev_char = ' ' #previous char
-with open(file_name,'r',encoding='utf-8') as file:
-    for line in file: # read line in our file
-        for char in line: # read char in our file
-            if prev_char in stat: # if we already have char in the dictionary, we increase the vale
-                if char in stat[prev_char]:
-                    stat[prev_char][char] +=1
+analysis_count = 4  # count of sequence
+
+sequence = ' ' * analysis_count  # previous char
+with open(file_name, 'r', encoding='utf-8') as file:
+    for line in file:  # read line in our file
+        line = line[:-1]
+        for char in line:  # read char in our file
+            if sequence in stat:  # if we already have char in the dictionary, we increase the vale
+                if char in stat[sequence]:
+                    stat[sequence][char] += 1
                 else:
-                    stat[prev_char][char] = 1
+                    stat[sequence][char] = 1
             else:
-                stat[prev_char] = {char:1}
-            prev_char = char # in the previous char we enter the one that was in front of it
+                stat[sequence] = {char: 1}
+            sequence = sequence[1:] + char  # in the previous char we enter the one that was in front of it
 
-totals = {} # dictionary to count all char for prev_char
+totals = {}  # dictionary to count all char for prev_char
 stat_for_generate = {}
-for prev_char, char_stat in stat.items():
-    totals[prev_char] = 0
-    stat_for_generate[prev_char] = []
-    for char,count in char_stat.items():
-        totals[prev_char] += count
-        stat_for_generate[prev_char].append([count,char])
-    stat_for_generate[prev_char].sort(reverse=True)
+for sequence, char_stat in stat.items():
+    totals[sequence] = 0
+    stat_for_generate[sequence] = []
+    for char, count in char_stat.items():
+        totals[sequence] += count
+        stat_for_generate[sequence].append([count, char])
+    stat_for_generate[sequence].sort(reverse=True)
 
-
-
-N = 1000 # volume char
+N = 1000  # volume char
 printed = 0
 
-prev_char =' '
+sequence = ' ' * analysis_count
+spaces_printed = 0
 while printed < N:
-    char_stat = stat_for_generate[prev_char]
-    total = totals[prev_char]
-    dice = randint(1,total)
+    char_stat = stat_for_generate[sequence]
+    total = totals[sequence]
+    dice = randint(1, total)
     pos = 0
-    for count,char in char_stat:
+    for count, char in char_stat:
         pos += count
         if dice <= pos:
             break
-    print(char,end='')
+    print(char, end='')
+    if char == ' ':
+        spaces_printed += 1
+        if spaces_printed >= 10:
+            print()
+            spaces_printed = 0
     printed += 1
-    prev_char = char
+    sequence = sequence[1:] + char
